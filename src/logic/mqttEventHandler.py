6e287ded567f583c4a2d79c632374937eec26f0d
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-03-31 11:49:02 +0200

    Added timestamp in message and small refactoring

--- src/logic/mqttEventHandler.py
+++ src/logic/mqttEventHandler.py
@@ -1,9 +1,10 @@
 import os
 from utils.logger import getLogger
+from datetime import datetime
 
 LEAK_DETECTION_MESSAGE = "The WATER detector in the Business Entry has detected water leakage !!!"
 FIRE_ALARM_MESSAGE = "The FIRE alarm has detected FIRE and/or SMOKE !!!"
-INTRUSION_ALARM_MESSAGE = "The INTRUSION alarm has detected a tripped sensor, which set of the alarm !!!"
+INTRUSION_ALARM_MESSAGE = "The INTRUSION alarm has detected a tripped sensor, which set off the alarm !!!"
 GATE_BELL_MESSAGE = "GATE Bell has been pressed. Who is at the gate?"
 DOOR_BELL_MESSAGE = "DOOR Bell has been pressed. Who is at the door?"
 
@@ -44,31 +45,27 @@ class MqttEventHandler:
             if deviceName != "GateState" and deviceValue != 1:
                 self.logger.debug(f"No need to process value: {deviceValue}")
                 return
-            await handler(payload)
+            message, image = await handler(payload)
+            await self._sendAppMessage(message, image)
         except Exception as e:
             self.logger.error(f"Error processing MQTT message: {e}")
 
     async def _handleLeakDetection(self, _):
-        message = f'{LEAK_DETECTION_MESSAGE}'
-        await self._sendAppMessage(message)
+        return LEAK_DETECTION_MESSAGE, None
 
     async def _handleFireAlarm(self, _):
-        message = f'<html><body>{FIRE_ALARM_MESSAGE}'
-        await self._sendAppMessage(message)
+        return FIRE_ALARM_MESSAGE, None
 
     async def _handleIntrusionAlarm(self, _):
-        message = f'{INTRUSION_ALARM_MESSAGE}'
-        await self._sendAppMessage(message)
+        return INTRUSION_ALARM_MESSAGE, None
 
     async def _handleGateBell(self, _):
-        message = f'{GATE_BELL_MESSAGE}'
         image = await self.cameraConnection.getCameraImage('garage') if self.cameraConnection else None
-        await self._sendAppMessage(message, image)
+        return GATE_BELL_MESSAGE, image
 
     async def _handleDoorBell(self, _):
-        message = f'{DOOR_BELL_MESSAGE}'
         image = await self.cameraConnection.getCameraImage('doorentry') if self.cameraConnection else None
-        await self._sendAppMessage(message, image)
+        return DOOR_BELL_MESSAGE, image
 
     async def _handleGateState(self, payload):
         state = payload.get("svalue")
@@ -83,8 +80,12 @@ class MqttEventHandler:
         if not self.domoticzAppAPI:
             self.logger.error("App connection not configured")
             return
-        payload = {'type': 'notification', 'message': message}
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        message_with_timestamp = f"[{timestamp}] {message}"
+
+        payload = {'type': 'notification', 'message': message_with_timestamp}
         if imageData:
             payload['imageData'] = imageData
         await self.domoticzAppAPI.broadcastMessage(payload)
-        self.logger.info(f"Message broadcasted, with messageText: {message}")
+        self.logger.info(f"Message broadcasted, with messageText: {message_with_timestamp}")
