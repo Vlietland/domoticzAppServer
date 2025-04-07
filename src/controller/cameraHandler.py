f051b8fa2df1242e74ac1837fe676d65c2068a17
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-07 20:16:44 +0200

    enabled camera feed

--- src/controller/cameraHandler.py
+++ src/controller/cameraHandler.py
@@ -11,7 +11,7 @@ class CameraEventHandler:
         imageData = await self.getCameraImage(cameraId)
         if imageData:
             responsePayload = {"type": "cameraImage", "cameraId": cameraId, "imageData": imageData}
-            #await self.broadcastMessage(responsePayload)
+            await self.broadcastMessage(responsePayload)
             self.logger.info(f"Retrieved and published image from camera {cameraId}")
         else:
             self.logger.warning(f"No image data available from camera {cameraId}")
