4df29da594c9e32222cfd8fc07fb5e9f48e22281
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-07 10:12:41 +0200

    moved queue to list with timestamps

--- src/controller/mqttMessageHandler.py
+++ src/controller/mqttMessageHandler.py
@@ -2,11 +2,11 @@ import os
 from utils.logger import getLogger
 
 class MqttMessageHandler:
-    def __init__(self, getGateDevice, setGateState, storeNotification, onNotification):
+    def __init__(self, getGateDevice, setGateState, storeAlert, onNotification):
         self.__logger = getLogger(__name__)
         self.__getGateDevice = getGateDevice
         self.__setGateState = setGateState
-        self.__storeNotification = storeNotification
+        self.__storeAlert = storeAlert
         self.__onNotification = onNotification
 
     def onMqttMessageCallback(self, topic, payload):
@@ -27,7 +27,7 @@ class MqttMessageHandler:
     def __handleNotificationDevice(self, deviceName, payload):
         nvalue = payload.get("nvalue")
         if nvalue == 1:
-            self.__storeNotification(deviceName)
+            self.__storeAlert(deviceName)
             self.__onNotification(deviceName)
             self.__logger.info(f"Device notification stored in the notifiation queue: {deviceName}")
         else:
