97a648286d0c0456c4f5aaeed77e6c295603f992
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-11 22:23:34 +0200

    update method namingn

--- src/controller/appMessageHandler.py
+++ src/controller/appMessageHandler.py
@@ -2,21 +2,21 @@ import os
 from utils.logger import getLogger
 
 class AppMessageHandler:
-    def __init__(self, handleGetNotificationsRequest, handleDeleteNotificationsRequest,
-                       handleCameraImageRequest, handleGateOpenRequest):
+    def __init__(self, onGetNotificationsRequest, onDeleteNotificationsRequest,
+                       onCameraImageRequest, onGateOpenRequest):
         self.__logger = getLogger(__name__)
-        self.__handleGetNotificationsRequest = handleGetNotificationsRequest
-        self.__handleDeleteNotificationsRequest = handleDeleteNotificationsRequest        
-        self.__handleCameraImageRequest = handleCameraImageRequest
-        self.__handleGateOpenRequest = handleGateOpenRequest
+        self.__onGetNotificationsRequest = onGetNotificationsRequest
+        self.__onDeleteNotificationsRequest = onDeleteNotificationsRequest        
+        self.__onCameraImageRequest = onCameraImageRequest
+        self.__onGateOpenRequest = onGateOpenRequest
 
     async def onAppMessageCallback(self, payload):
         messageType = payload.get('type')
         if messageType == "getAlerts":
-            self.__handleGetNotificationsRequest()
+            self.__onGetNotificationsRequest()
         elif messageType == "purgeAlerts":
-            self.__handleDeleteNotificationsRequest()
+            self.__onDeleteNotificationsRequest()
         elif messageType == "getCameraImage":
-            await self.__handleCameraImageRequest(payload)
+            await self.__onCameraImageRequest(payload)
         elif messageType == "openGateCommand":
-            await self.__handleGateOpenRequest(payload)
+            await self.__onGateOpenRequest(payload)
