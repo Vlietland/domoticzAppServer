5cbc58aa617459dc366c2dc8cd0cf91137ace7ab
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-13 10:04:33 +0200

    removed image log printing

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,15 +4,6 @@ import os
 
 load_dotenv()
 LOG_LEVEL = os.getenv("LOG_LEVEL")
-MAX_LOG_LENGTH = 2000
-
-class TruncatingLogger(logging.Logger):
-    def _log(self, level, msg, args, **kwargs):
-        if isinstance(msg, str) and len(msg) > MAX_LOG_LENGTH:
-            msg = msg[:MAX_LOG_LENGTH] + "... [truncated]"
-        super()._log(level, msg, args, **kwargs)
-
-logging.setLoggerClass(TruncatingLogger)
 
 logging.basicConfig(
     level=LOG_LEVEL,
