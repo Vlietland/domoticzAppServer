d56cc8b26ee316b4ec9b49cefb25b7628deeb15e
Jan Vlietland <j.vlietland@search4solutions.nl>
2025-04-13 09:24:43 +0200

    added weather information

--- src/controller/appMessageHandler.py
+++ src/controller/appMessageHandler.py
@@ -3,13 +3,14 @@ from utils.logger import getLogger
 
 class AppMessageHandler:
     def __init__(self, onGetNotificationsRequest, onDeleteNotificationsRequest,
-                       onCameraImageRequest, onGateOpenRequest, onGateCloseRequest):
+                       onCameraImageRequest, onGateOpenRequest, onGateCloseRequest, onWeatherRequest):
         self.__logger = getLogger(__name__)
         self.__onGetNotificationsRequest = onGetNotificationsRequest
         self.__onDeleteNotificationsRequest = onDeleteNotificationsRequest        
         self.__onCameraImageRequest = onCameraImageRequest
         self.__onGateOpenRequest = onGateOpenRequest
         self.__onGateCloseRequest = onGateCloseRequest        
+        self.__onWeatherRequest = onWeatherRequest                
 
     async def onAppMessageCallback(self, payload):
         messageType = payload.get('type')
@@ -23,3 +24,6 @@ class AppMessageHandler:
             await self.__onGateOpenRequest(payload)
         elif messageType == "closeGateCommand":
             await self.__onGateCloseRequest(payload)            
+        elif messageType == "getWeather":
+            await self.__onWeatherRequest(payload)
+        else self.logger.info(f"Request unable to process: {mqttPayload}")
--- src/controller/weatherHandler.py
+++ src/controller/weatherHandler.py
@@ -6,14 +6,27 @@ class WeatherHandler:
         self.logger = getLogger(__name__)
         self.enqueueMessage = enqueueMessage
         self.weatherDevice = os.getenv('DEVICE_7')
+        self.temperature = None
 
     def getWeatherDevice(self):
         return self.weatherDevice
 
     async def onWeatherDataReceived(self, temp):
         if temp is not None:
+            self.temperature = temp
             message = {'type': 'weather', 'outsideTemp': temp}
             self.enqueueMessage(message)
             self.logger.info(f"Retrieved and enqueued weather data: {temp}")
         else:
             self.logger.warning(f"No valid weather data received (temp is None)")
+
+    async def onWeatherRequest(self):
+        if self.temperature == None:
+            self.logger.debug("Temperature not yet received")
+            return
+        message = {'type': 'weather', 'temp': temperature}
+        try:
+            self.__enqueueMessage(message)
+            self.__logger.info("Weather information enqueued for transfer to client.")
+        except Exception as e:
+            self.__logger.error(f"Failed to enqueue weather message: {e}")
